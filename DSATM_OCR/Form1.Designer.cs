e3a85fc72cdad8635737a85ef71bf6dfd007d676
Archisol <rahulreddy93m@gmail.com>
2018-07-28 22:06:33 +0530

    first commit

---
+++ DSATM_OCR/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DSATM_OCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(352, 31);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(284, 245);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Extract Features for all images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(384, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Train SVM Model";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Test Button";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(648, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 55);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DSATM_OCR/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using FeatureExtraction;
+using System.IO;
+using SVM;
+
+namespace DSATM_OCR
+{
+    public partial class Form1 : Form
+    {
+        Bitmap originalImage;
+        Model model;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            if(openDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openDialog.FileName);
+                pictureBox1.Image = originalImage;
+            }
+        }
+
+        private Bitmap preProcess(Bitmap originalImage)
+        {
+            Invert invertObj = new Invert();
+            Bitmap invertImage = invertObj.Apply((Bitmap)originalImage.Clone());
+
+            pictureBox2.Image = invertImage;
+
+            invertImage = Grayscale.CommonAlgorithms.BT709.Apply(invertImage);
+            Threshold bwObject = new Threshold();
+            invertImage = bwObject.Apply(invertImage);
+            ExtractBiggestBlob blobObject = new ExtractBiggestBlob();
+            invertImage = blobObject.Apply(invertImage);
+
+            pictureBox2.Image = invertImage;
+
+            ResizeBicubic resize = new ResizeBicubic(60, 90);
+
+            invertImage = resize.Apply(invertImage);
+
+            /*CannyEdgeDetector edgeDetector = new CannyEdgeDetector();
+            invertImage = edgeDetector.Apply(invertImage);*/
+
+            return invertImage;
+        }
+
+        private void WriteToFile(List<float[]> features, int label, ref StreamWriter sw)
+        {
+
+
+            foreach (var featureVector in features)
+            {
+                sw.Write(label + " ");
+                for (int index = 0; index < featureVector.Length; index++)
+                {
+                    sw.Write((index + 1) + ":" + featureVector[index] + " ");
+                }
+                sw.WriteLine();
+            }
+
+
+
+            sw.Flush();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderDialog.SelectedPath;
+
+                HOEF hoefObj = new HOEF();
+
+                FileStream fs = new FileStream("Train", FileMode.Create, FileAccess.Write);
+
+                StreamWriter sw = new StreamWriter(fs);
+
+                for(int index = 0;index<4;index++)
+                {
+                    char subFolder = (char)('a' + index);
+                    string finalFolder = folderPath + "\\" + subFolder;
+
+                    string[] allfiles = Directory.GetFiles(finalFolder);
+
+                    List<float[]> features = new List<float[]>();
+
+                    for (int i = 0; i < allfiles.Length; i++)
+                    {
+                       
+                            Bitmap img = new Bitmap(allfiles[i]);
+                            img = preProcess((Bitmap)img.Clone());
+
+                        
+                        float[] featureVector = hoefObj.Apply(img);
+                        features.Add(featureVector);
+
+
+                    }
+
+                    WriteToFile(features, index, ref sw);
+                    
+                }
+                
+                sw.Close();
+                fs.Close();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Problem train = Problem.Read("Train");
+            Parameter parameters = new Parameter();
+            parameters.C = 32; parameters.Gamma = 8;
+            model = Training.Train(train, parameters);
+            MessageBox.Show("model is trained");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            if(openDialog.ShowDialog()== DialogResult.OK)
+            {
+                Bitmap img = new Bitmap(openDialog.FileName);
+                pictureBox1.Image = img;
+                img = preProcess((Bitmap)img.Clone());
+
+                HOEF hoefObj = new HOEF();
+                float[] featureVector = hoefObj.Apply(img);
+
+                List<float[]> features = new List<float[]>();
+                features.Add(featureVector);
+
+
+                FileStream fs = new FileStream("Test", FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+               
+
+
+                WriteToFile(features, 50, ref sw);
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+
+                Problem test = Problem.Read("Test");
+                Prediction.Predict(test, "result", model, false);
+
+                FileStream fsRead = new FileStream("result", FileMode.Open, FileAccess.Read);
+
+                StreamReader sr = new StreamReader(fsRead);
+                string result = sr.ReadLine();
+
+                sr.Close();
+                fsRead.Close();
+
+                int iResult = Int32.Parse(result);
+
+
+                char[] lookuptable = { 'A', 'B', 'C', 'D' };
+                string output = lookuptable[iResult].ToString();
+                label1.Text = output.ToString();
+            }
+        }
+    }
+}
